86acf65b07eaf5b1dfd4f74d2a07e9af119afe30
ochristi <o0.christi+dev@gmail.com>
2011-04-17 02:05:50 +0200

    direction script

---
+++ dir.js
@@ -0,0 +1,12 @@
+/**
+ * craftscript
+ * returns the players viewing direction
+ */
+
+importPackage(Packages.java.io);
+importPackage(Packages.java.awt);
+importPackage(Packages.com.sk89q.worldedit);
+importPackage(Packages.com.sk89q.worldedit.blocks);
+importPackage(Packages.com.sk89q.worldedit.util);
+
+player.print(player.getCardinalDirection());
